f47a6e533e66c6d0025a5329fe519f5e35a60c37
Rishit-21 <rishit@webtheuniverse.com>
2022-07-08 18:21:56 +0530

    soo not done

--- todo.js
+++ todo.js
@@ -20,6 +20,7 @@ let add_btn = document.querySelector('.add-btn');
 let search_btn = document.querySelector('.search-btn');
 
 let action_btn = document.querySelector('.action-btn')
+let action = document.querySelector('.actionS')
 let select = document.querySelector('.select')
 let unSelect = document.querySelector('.un-select')
 let deleteSelected = document.querySelector('.deletedselect')
@@ -39,120 +40,95 @@ let btn_1=document.querySelector('.btn--1');
 let btn_2=document.querySelector('.btn--2');
 let  active
 
-let taskArray=[{}];
+let taskArray=[];
+let array=[]
 taskList.innerHTML = '';
 document.querySelector('.taskmain').innerHTML = '';
-let cChecked=check.checked;
-
-let hTml=function(e){
-    const html=`
-    <div class="taskList">
+let cChecked=check
+let input
+const displayTask=function(taskArray){
+    document.querySelector('.taskmain').innerHTML = '';
+    taskArray.forEach(item=>{
+        console.log(item)
+        const html=`
+        <div class="taskList">
         <div class = 'taskName'>
-            <input type="checkbox">${e}<span><input type="textbox" value="${e}"></span> 
+            <input type="checkbox" class="check" onclick=checks(item.id) >${item.name} 
+            <span><input type="textbox" value="${item.name}"></span> 
         </div>
         <div class="edits">
             <button class="edit"><img src="edit.svg" alt="edit" class="editIcon"></button>
             <button class="delete"><i class="fa-solid fa-delete-left deletebtn"></i></button>
-        </div>
-</div>`; 
-document.querySelector('.taskmain').insertAdjacentHTML('beforeend',html);
-}
-
-const addEl =function(){
-    var value1 = text.value;
-    //console.log(text.value);
-   value1= value1.trim()
-   if(value1){
-    
-    taskArray.push({id:Math.trunc(Math.random()*1000),taskName:value1,cChecked:false});
-    
-        hTml(value1)
-    //taskList.style.display='flex';
-   }
-    //console.log(taskArray)
-}
-
-const searchEl = function(){
-    var value1 = text.value;
-    //console.log(text.value);
-   value1= value1.toLowerCase().trim()
-    let searchitem= taskArray.map(el=>el.taskName==value1?value1:`No data found`)
-    let filterItem = searchitem.filter(el=>el==value1)
-    console.log(filterItem)
-    if(filterItem.includes(value1)){
-        value1='No Data Found'
-    }
-    else{
-        value1=filterItem.join('')
-    }
-    console.log(searchitem)
-    const html=`
-    <div class="taskList">
-        <div class = 'taskName'>
-            <input type="checkbox">${value1}<span><input type="textbox" value="${value1}"></span> 
-        </div>
-        <div class="edits">
-            <button class="edit"><img src="edit.svg" alt="edit" class="editIcon"></button>
-            <button class="delete"><i class="fa-solid fa-delete-left deletebtn"></i></button>
-        </div>
-</div>`; 
-console.log(searchitem)
-    
-    //hTml(searchitem)
-
-document.querySelector('.taskmain').innerHTML=html;
+        </div>`
+        document.querySelector('.taskmain').insertAdjacentHTML('beforeend',html);
+    })
 }
 
 
-add_btn.addEventListener('click',function(e){
-   //e=0
-
-    active=0;
+add_btn.addEventListener('click',function(){
+    active=0
     add_btn.classList.add('btn--active')
-    search_btn.classList.remove('btn--active')    
-    // e.preventDefault()
-    box.style.display='block';
-
+    search_btn.classList.remove('btn--active')
+    box.style.display='block'
+})
+search_btn.addEventListener('click',function(){
+    active=1;
+    add_btn.classList.remove('btn--active')
+    search_btn.classList.add('btn--active')
+    //box.style.display='block'
 })
 
+function checks(i){
+    taskArray.forEach(el=>{
+        if(el.id==i){
+            if(!el.cCheck){
+                check.checked=true;
+                el.cCheck=true;
+                
+            }
+            else{
+                check.checked=false;
+                el.cCheck=false;
+            }
 
-search_btn.addEventListener('click',function(e){
-    active=1
-    search_btn.classList.add('btn--active');
-    add_btn.classList.remove('btn--active');
-   
-    // value1=text.value
-    // if(taskArray.includes(value1)){
+        }
+        console.log(taskArray)
+    })
+}
 
-    // document.querySelector('.taskmain').insertAdjacentHTML('beforeend',html);
-
-    // }
+const add = function(){
+    input=text.value;
+    let checked = false
+    input=input.trim()
+    if(input){
+        taskArray.push({id:Math.trunc(Math.random()*1000),name:input,cCheck:checked})
+        console.log(taskArray)
+        displayTask(taskArray)
+    }
+    console.log(taskArray)
+}
+const search =function(){
+    input=text.value
+    input=input.toLowerCase()
+    const searched = taskArray.filter(el=>el.name==input)
+        displayTask(searched)
+        console.log(searched)
     
-})
+}
+//add(taskArray)
 
-
-
-
-boxBtn.addEventListener('click',function(){
-    console.log(active)
-    boxBtn.addEventListener('keydown' ,function(e){
-        // e.preventDefault();
-        if(active==0){
-            if(e.key==='Enter'){
-                addEl();
-              
-        
+// boxBtn.addEventListener('click',function(){
+    boxBtn.addEventListener('keyup',function(e){
+        if(e.key=='Enter'){
+            if(active==0){
+                console.log("active=0")
+                add(taskArray);
+                text.value='';
+            }
+            else if(active==1){
+                search();
             }
         }
-            else{
-                if(e.key==='Enter'){
-
-                    searchEl()
-                }
-            }
-            text.value=''
-        })
-})
-
-
+    })
+// })
 
